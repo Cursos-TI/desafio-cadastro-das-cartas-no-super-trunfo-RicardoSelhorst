d37f9eeb43e8f46dc579eadc79c8f2d836415a68
rick <202503056692@alunos.estacio.br>
2025-02-25 19:29:56 -0300

    Add files via upload

--- supertrunfo2.c
+++ supertrunfo2.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
  
 int main(){
-    int populacao = 1829225;
-    float pib = 192.7;
-    float area = 435.277;
+    float populacao = 1829225;
+    float pib = 1927000000;
+    float area = 4352777;
     char estado[50] = "B";
     char cidade[20] = "Curitiba";
-    char tipo[50] = "B02";
+    char codigo[50] = "B02";
     int turi = 20;
- 
-    printf("Estado: %s\nCódigo da Carta: %s\n", estado, tipo);
-    printf("Nome da cidade: %s\nPopulação: %d\n", cidade, populacao);
-    printf("Area da Cidade: %2.f.277 km²\nPIB: %2.f.7 bilhões de reais\n", area, pib);
+    float densidade = populacao / area;
+    float capital= pib / populacao;
+    printf("Estado: %s\nCódigo: %s\n", estado, codigo);
+    printf("Nome da cidade: %s\nNúmero de habitantes: %2.f Milhões de habitantes\n", cidade, populacao);
+    printf("Área da Cidade: %2.f km²\nPIB: %2.f Bilhões de reais\n", area, pib);
     printf("Número de Pontos Turísticos: %d\n", turi);
+    printf("Densidade: %.3f hab/Km²\nCapital %.2f de reais\n", densidade, capital);
+
+
 }
